afff5c5afc9b47b76006d162784abfda6083ca4b
Giulio G <36572507+julio77it@users.noreply.github.com>
2020-11-06 12:39:26 +0100

    static compilation
    CGO_FLAGS applied in building script, permitting to static link the tibrv libraries

--- scripts/build.bash
+++ scripts/build.bash
@@ -3,11 +3,10 @@
 source test_profile
 
 cd ..
-go build -trimpath 
-go build -trimpath examples/simple/simple.go
-go build -trimpath examples/rvlisten/rvlisten.go
-go build -trimpath examples/rvsend/rvsend.go
-go build -trimpath examples/sendrequest/sendrequest.go
-go build -trimpath examples/sendreply/sendreply.go
-go build -trimpath examples/json/json.go
-
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath 
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath examples/simple/simple.go
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath examples/rvlisten/rvlisten.go
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath examples/rvsend/rvsend.go
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath examples/sendrequest/sendrequest.go
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath examples/sendreply/sendreply.go
+go build -ldflags "-extldflags '$CGO_FLAGS'" -a -trimpath examples/json/json.go
